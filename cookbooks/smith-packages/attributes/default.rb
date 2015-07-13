736d96afb3d6f586187c5665ef89a9aade4fa607
Tim Smith <tim@cozy.co>
2015-07-12 23:57:33 -0700

    Add zlib1g-dev as well

--- cookbooks/smith-packages/attributes/default.rb
+++ cookbooks/smith-packages/attributes/default.rb
@@ -1,2 +1,2 @@
-default['smith-packages']['install'] = %w(iotop unzip curl wget ruby1.9.1-dev libxml2-dev)
+default['smith-packages']['install'] = %w(iotop unzip curl wget ruby1.9.1-dev libxml2-dev zlib1g-dev)
 default['smith-packages']['remove'] = %w(popularity-contest whoopsie landscape-common command-not-found command-not-found-data)
