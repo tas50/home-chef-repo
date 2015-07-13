809433077c1782e47ca85509aeae1288160a9bdf
Tim Smith <tim@cozy.co>
2015-07-12 23:07:34 -0700

    Use the ruby dev package for native gem compiles

--- cookbooks/smith-packages/attributes/default.rb
+++ cookbooks/smith-packages/attributes/default.rb
@@ -1,2 +1,2 @@
-default['smith-packages']['install'] = %w(iotop unzip curl wget ruby1.9.3)
+default['smith-packages']['install'] = %w(iotop unzip curl wget ruby1.9.1-dev)
 default['smith-packages']['remove'] = %w(popularity-contest whoopsie landscape-common command-not-found command-not-found-data)
