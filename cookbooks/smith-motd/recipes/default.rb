cb8739d4fc4e2fc52bda305555159fe4ef8d1f44
Tim Smith <tsmith84@gmail.com>
2015-10-23 21:57:57 -0700

    Copyright and license updates

--- cookbooks/smith-motd/recipes/default.rb
+++ cookbooks/smith-motd/recipes/default.rb
@@ -2,15 +2,25 @@
 # Cookbook Name:: smith-motd
 # Recipe:: default
 #
-# Copyright (C) 2014 Tim Smith
+# Copyright 2014-2015, Tim Smith
 #
-# All rights reserved - Do Not Redistribute
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
 
 package 'update-motd' if node.platform == 'ubuntu'
 
 # make sure the dir is there on debian
-directory '/etc/update-motd.d'  
+directory '/etc/update-motd.d'
 
 # remove fluff from motd
 %w(95-hwe-eol 91-release-upgrade 51-cloudguest 98-cloudguest 10-help-text 50-landscape-sysinfo).each do |motd_file|
--- cookbooks/smith-packages/recipes/default.rb
+++ cookbooks/smith-packages/recipes/default.rb
@@ -2,9 +2,19 @@
 # Cookbook Name:: smith-packages
 # Recipe:: default
 #
-# Copyright 2013, Tim Smith
+# Copyright 2013-2015, Tim Smith
 #
-# All rights reserved - Do Not Redistribute
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
 
 node['smith-packages']['install'].each do |pkg|
@@ -14,5 +24,5 @@ end
 node['smith-packages']['remove'].each do |pkg|
   package pkg do
    action :remove
-  end 
+  end
 end
