2bab1909c48b89d5b7d13149b741c4a07720a7f0
Tim Smith <tsmith@chef.io>
2016-05-21 19:57:41 -0700

    Dep updates
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- cookbooks/role-base/metadata.rb
+++ cookbooks/role-base/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'tsmith84@gmail.com'
 license          'Apache 2.0'
 description      'Base role cookbook for all nodes'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.3.0'
+version          '0.3.1'
 
 depends 'apt'
 depends 'smith-packages'
