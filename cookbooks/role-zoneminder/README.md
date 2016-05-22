1f65bf06b65519c261f3ad4c7529c6fbb9eb6e36
Tim Smith <tsmith@chef.io>
2016-05-21 23:38:09 -0700

    Add a zoneminder role
    Signed-off-by: Tim Smith <tsmith@chef.io>

---
+++ cookbooks/role-zoneminder/README.md
@@ -0,0 +1,3 @@
+# role-zoneminder Cookbook
+
+This is the role cookbook for the zoneminder security camera software.
---
+++ cookbooks/role-zoneminder/metadata.rb
@@ -0,0 +1,9 @@
+name             'role-zoneminder'
+maintainer       'Tim Smith'
+maintainer_email 'tsmith84@gmail.com'
+license          'Apache 2.0'
+description      'Role cookbook for the monitoring host'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends 'zoneminder'
