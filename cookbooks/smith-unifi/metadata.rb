9b2ae61b227bfb750751c38cbf03136cf3d1b82a
Tim Smith <tsmith@chef.io>
2016-05-29 20:58:09 -0700

    Use my zoneminder fork
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- cookbooks/smith-unifi/metadata.rb
+++ cookbooks/smith-unifi/metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'tsmith84@gmail.com'
 license          'Apache 2.0'
 description      'Wrapper cookbook for installing unifi controller'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
 depends 'unifi'
