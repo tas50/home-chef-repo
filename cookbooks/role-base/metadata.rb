dcf871e1abc72575f9c2b23cb91ed912a49a03e1
Tim Smith <tsmith@chef.io>
2017-08-17 15:41:27 -0700

    Make the base role include sensu client setup
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- cookbooks/role-base/metadata.rb
+++ cookbooks/role-base/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'tsmith84@gmail.com'
 license          'Apache-2.0'
 description      'Base role cookbook for all nodes'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.4.1'
+version          '0.4.2'
 
 depends 'smith-packages'
 depends 'smith-motd'
@@ -14,6 +14,7 @@ depends 'sensors'
 depends 'build-essential'
 depends 'vim'
 depends 'chef_client_updater'
+depends 'smith-sensu'
 
 chef_version '>= 12'
 supports 'debian'
